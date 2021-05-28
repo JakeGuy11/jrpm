dccce772167a2dbdbe43da18d9a3ef0f4b3bedb5
Jake <Jake_Guy_11@protonmail.ch>
2021-05-28 10:22:54 -0400

    added file detection for add_site

--- src/main.rs
+++ src/main.rs
@@ -44,12 +44,26 @@ fn main()
             }
             else { println! ("Debug mode must be enabled to use this feature."); }
         },
+        "add-site" =>
+        {
+            if DEBUG_MODE
+            {
+                let site_return = add_site();
+                match site_return
+                {
+                    0 => println! ("Your password was sucessfully saved!"),
+                    1 => println! ("Password creation aborted"),
+                    _ => eprintln! ("unrecognized return!")
+                }
+            }
+            else { println! ("Debug mode must be enabled to use this feature."); }
+        },
         "init" =>
         {
             let init_return = init();
             match init_return
             {
-                0 => println! ("jrpm was succesfully initialized!"),
+                0 => println! ("JPRM was succesfully initialized!"),
                 1 => eprintln! ("Initialization aborted."),
                 _ => eprintln! ("unrecognized return!")
             }
@@ -58,13 +72,42 @@ fn main()
     }
 }
 
+fn add_site() -> i32
+{
+    // Prompt the user for the site they would like to save for
+    println! ("=============================================");
+    println! ("Passwords are stored by JPRM using a keyword;");
+    println! ("It is suggested that you use a common keyword");
+    println! ("so you don't forget, such as `gmail` for a");
+    println! ("gmail account.");
+    println! ("Enter the keyword you would like to associate");
+    println! ("with this password:");
+    let mut site_keyword = String::new();
+    std::io::stdin().read_line(&mut site_keyword).unwrap_or(0);
+    println! ("=============================================");
+    let file_name: String = site_keyword.trim().to_owned() + ".jrpm";
+    let mut file_full_path = env::home_dir().expect("Home not found!");
+    file_full_path.push(".jrpm");
+    file_full_path.push(&file_name);
+    if file_full_path.is_file()
+    {
+        println! ("IMPORTANT: you already have a password saved under that keyword. Would you like to overwrite it? [y/N]");
+        let mut user_confirmation = String::new();
+        let continue_pass = String::from("y");
+        std::io::stdin().read_line(&mut user_confirmation).unwrap();
+        if user_confirmation.trim().ne(&continue_pass) { return 1; }
+        else { println! ("You've selected to overwrite your password."); }
+    }
+    0
+}
+
 fn init() -> i32
 {
     // Check if the directory exists. If it doesn't, create it
     let mut dir_path = env::home_dir().expect("Home not found!");
     dir_path.push(".jrpm");
     if dir_path.is_dir() { println! ("{:?} already exists; continuing with initiation...", dir_path); }
-    else { fs::create_dir_all(dir_path); }
+    else { if let Err(e) = fs::create_dir_all(dir_path) { eprintln! ("Failed to create directory: {:?}", e); } }
     
     // Check if the key exists. If it does and the user wants to overwrite it, rename it to
     // .key.old and create a new key. If it doesn't exist, just make a new key
@@ -76,16 +119,17 @@ fn init() -> i32
         println! ("IMPORTANT: YOU HAVE AN EXISTING KEY\nIf you choose to continue, it will be overwrited, making your passwords inaccesable.\nType \"cONFIRm\" to continue, type anything else to cancel.");
         let mut user_confirmation = String::new();
         let continue_pass = String::from("cONFIRm");
-        let reader = std::io::stdin().read_line(&mut user_confirmation).unwrap_or(0);
+        std::io::stdin().read_line(&mut user_confirmation).unwrap_or(0);
         if user_confirmation.trim().ne(&continue_pass) { return 1; }
         else { println! ("You've selected to overwrite your key. Good luck with that."); }
+        let mut new_key_path = key_path.clone();
+        new_key_path.pop();
+        new_key_path.push(".key.old");
+        if let Err(e) = fs::rename(&key_path, new_key_path) { eprintln! ("Failed to move .key to .key.old: {:?}", e); }
     }
-    else
-    {
-        let key_to_write = crypto::Encoder::generate_new_key();
-        let key_as_string: String = key_to_write.into_iter().map(|i| i.to_string()).collect::<String>();
-        fs::write(key_path, &key_as_string);
-    }
+    let key_to_write = crypto::Encoder::generate_new_key();
+    let key_as_string: String = key_to_write.into_iter().map(|i| i.to_string()).collect::<String>();
+    if let Err(e) = fs::write(key_path, &key_as_string) { eprintln! ("Failed to write to file: {:?}", e); }
     0
 }
 
