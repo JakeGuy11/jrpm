f07145a991e8b258846fcff3faa92b9912caf12e
Jake <Jake_Guy_11@protonmail.ch>
2021-05-27 23:59:53 -0400

    added dir and key file initialization

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 use std::env;
 use std::path::PathBuf;
+use std::fs;
 mod crypto;
 
 fn main()
@@ -43,7 +44,48 @@ fn main()
             }
             else { println! ("Debug mode must be enabled to use this feature."); }
         },
+        "init" =>
+        {
+            let init_return = init();
+            match init_return
+            {
+                0 => println! ("jrpm was succesfully initialized!"),
+                1 => eprintln! ("Initialization aborted."),
+                _ => eprintln! ("unrecognized return!")
+            }
+        },
         _ => {}
     }
+}
+
+fn init() -> i32
+{
+    // Check if the directory exists. If it doesn't, create it
+    let mut dir_path = env::home_dir().expect("Home not found!");
+    dir_path.push(".jrpm");
+    if dir_path.is_dir() { println! ("{:?} already exists; continuing with initiation...", dir_path); }
+    else { fs::create_dir_all(dir_path); }
+    
+    // Check if the key exists. If it does and the user wants to overwrite it, rename it to
+    // .key.old and create a new key. If it doesn't exist, just make a new key
+    let mut key_path = env::home_dir().expect("Home not found!");
+    key_path.push(".jrpm");
+    key_path.push(".key");
+    if key_path.is_file()
+    {
+        println! ("IMPORTANT: YOU HAVE AN EXISTING KEY\nIf you choose to continue, it will be overwrited, making your passwords inaccesable.\nType \"cONFIRm\" to continue, type anything else to cancel.");
+        let mut user_confirmation = String::new();
+        let continue_pass = String::from("cONFIRm");
+        let reader = std::io::stdin().read_line(&mut user_confirmation).unwrap_or(0);
+        if user_confirmation.trim().ne(&continue_pass) { return 1; }
+        else { println! ("You've selected to overwrite your key. Good luck with that."); }
+    }
+    else
+    {
+        let key_to_write = crypto::Encoder::generate_new_key();
+        let key_as_string: String = key_to_write.into_iter().map(|i| i.to_string()).collect::<String>();
+        fs::write(key_path, &key_as_string);
+    }
+    0
 }
 
